3c668f965db380da42867c04ef7a214a478e67e4
Ryan Honrado <ry1015@gmail.com>
2015-09-23 09:47:21 -0700

    removed 165 folder

--- 165/myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from myapp.models import UserProfile, ProviderProfile, Jobs
-
-# Register your models here.
-models = {UserProfile, ProviderProfile, Jobs}
-
-admin.site.register(models)
