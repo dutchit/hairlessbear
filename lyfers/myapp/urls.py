a8790aa7b7e25d0e0d2e5ea3f1dd9df2f9391898
Ryan Honrado <ry1015@gmail.com>
2015-10-08 10:35:16 -0700

    Deleted some functions

--- lyfers/myapp/urls.py
+++ lyfers/myapp/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from myapp import views
+from myapp.user import user
+from myapp.provider import provider
 
 urlpatterns = [
-    url(r'^api/userprofiles/$', views.userprofile_list),
-    url(r'^api/userprofile/(?P<pk>[0-9]+)/$', views.userprofile_detail),
-    url(r'^api/providerprofiles/$', views.providerprofile_list),
-    url(r'^api/providerprofiles/(?P<pk>[0-9]+)/$', views.user_providerprofile_list),
+    # url(r'^api/userprofiles/$', views.userprofile_list),
+    # url(r'^api/userprofile/(?P<pk>[0-9]+)/$', views.userprofile_detail),
+    url(r'^api/userprofiles/$', user.userprofile_list),
+    url(r'^api/userprofiles/(?P<pk>[0-9]+)/$', user.userprofile_detail),
+    url(r'^api/providerprofiles/$', provider.providerprofile_list),
+    url(r'^api/providerprofiles/(?P<pk>[0-9]+)/$', provider.user_providerprofile_list),
     url(r'^api/jobs/$', views.jobs_list),
     url(r'^api/jobs/(?P<pk>[0-9]+)/$', views.user_jobs_list),
     url(r'^api/jobs/(?P<pk>[0-9]+)/(?P<job_number>[0-9]+)/$', views.user_job_detail),
