edcd2ae499f60360413c220f5ad8b45a67dda00f
Ryan Honrado <ry1015@gmail.com>
2015-10-13 14:55:03 -0700

    Removed trailing slashes

--- lyfers/myapp/urls.py
+++ lyfers/myapp/urls.py
@@ -8,17 +8,17 @@ from myapp.job import job
 urlpatterns = [
     # url(r'^api/userprofiles/$', views.userprofile_list),
     # url(r'^api/userprofile/(?P<pk>[0-9]+)/$', views.userprofile_detail),
-    url(r'^api/userprofiles/$', user.userprofile_list),
-    url(r'^api/userprofiles/(?P<pk>[0-9]+)/$', user.userprofile_detail),
-    url(r'^api/providerprofiles(/*)$', provider.providerprofile_list),
-    url(r'^api/providerprofiles/(?P<pk>[0-9]+)/$', provider.user_providerprofile_list),
-    url(r'^api/providerprofiles/(?P<pk>[0-9]+)/(?P<providerprofile_number>[0-9]+)/$', provider.user_providerprofile_detail),
-    url(r'^api/jobs/$', job.jobs_list),
-    url(r'^api/jobs/(?P<pk>[0-9]+)/$', job.user_jobs_list),
-    url(r'^api/jobs/(?P<pk>[0-9]+)/(?P<job_number>[0-9]+)/$', job.user_job_detail),
-    url(r'^api/jobs/categories/$', job.categories_list),
-    url(r'^api/jobs/contracts/$', job.contract_list),
-    url(r'^api/userprofiles/preferences/$', views.preferences_list)
+    url(r'^api/userprofiles$', user.userprofile_list),
+    url(r'^api/userprofiles/(?P<pk>[0-9]+)$', user.userprofile_detail),
+    url(r'^api/providerprofiles$', provider.providerprofile_list),
+    url(r'^api/providerprofiles/(?P<pk>[0-9]+)$', provider.user_providerprofile_list),
+    url(r'^api/providerprofiles/(?P<pk>[0-9]+)/(?P<providerprofile_number>[0-9]+)$', provider.user_providerprofile_detail),
+    url(r'^api/jobs$', job.jobs_list),
+    url(r'^api/jobs/(?P<pk>[0-9]+)$', job.user_jobs_list),
+    url(r'^api/jobs/(?P<pk>[0-9]+)/(?P<job_number>[0-9]+)$', job.user_job_detail),
+    url(r'^api/jobs/categories$', job.categories_list),
+    url(r'^api/jobs/contracts$', job.contract_list),
+    url(r'^api/userprofiles/preferences$', views.preferences_list)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
