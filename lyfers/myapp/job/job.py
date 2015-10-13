d86ef2994ffaaa7c73d3e5f7882b9c934d6a7caf
Ryan Honrado <ry1015@gmail.com>
2015-10-12 20:02:20 -0700

    Fix issue

--- lyfers/myapp/job/job.py
+++ lyfers/myapp/job/job.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from myapp.models import UserProfile, Jobs
-from myapp.serializers import JobsSerializer
+from myapp.models import UserProfile, Jobs, Contract
+from myapp.serializers import JobsSerializer, ContractSerializer
 import json
 
 @api_view(['GET', 'POST'])
@@ -146,5 +146,55 @@ def user_job_detail(request, pk, job_number, format=None):
         job = Jobs.objects.get(id=job_number)
         job.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'POST'])
+def contract_list(request, format=None):
+    """
+    List all Contracts, or create a new Contract.
+
+    Path: /api/jobs/contracts/
+
+    POST PARAMETERS
+    data = {
+        "applicationID": "Application ID number",
+        "jobID": "Job ID number",
+        "status": "A status",
+        "job_posterID": "Job Poster ID number",
+        "job_poster_rating": "Job Poster Rating",
+        "job_applicantID": "Job Applicant ID number",
+        "job_applicant_rating": "Job Applicant Rating",
+        "payment": "Payment ID number",
+    }
+    """
+    if request.method == 'GET':
+        contracts = Contract.objects.all()
+        serializer = ContractSerializer(contracts, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        print ("Data: " + str(request.data))
+
+        try:
+            userID = request.data["userID"]
+            user = UserProfile.objects.get(id=job_posterID)
+        except:
+            print ("Username is not in the system.")
+            content = "Username is not in the system."
+            Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            serializer = ContractSerializer(data=request.data)
+        except:
+            print ("Data is wrong compared to Jobs Serializer.")
+            content = "Data is wrong compared to Jobs Profile Serializer."
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     return Response(status=status.HTTP_400_BAD_REQUEST)
