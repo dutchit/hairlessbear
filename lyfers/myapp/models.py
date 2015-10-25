30ccdc382517163b8fb6c187c45376957930874b
Ryan Honrado <ry1015@gmail.com>
2015-10-25 15:24:39 -0700

    added employer and employee rating into UserProfile

--- lyfers/myapp/models.py
+++ lyfers/myapp/models.py
@@ -13,7 +13,9 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
     last_name = models.CharField(max_length=200, blank=True)
     password = models.CharField(max_length=20, blank=True)
-
+    employer_rating = models.IntegerField(default=0, blank=True)
+    employee_rating = models.IntegerField(default=0, blank=True)
+    
     def __str__(self):
         return "(" + str(self.id) + ") "+ str(self.username)
 
